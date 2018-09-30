4cf2a027cb6fac043fb367d04921d7e6c9c3cb94
Palitokash <43505918+Palitokash@users.noreply.github.com>
2018-09-30 17:15:02 -0400

    Add files via upload
    P2HW2_Celsius Converter

---
+++ P2HW1_CelsiusConverter_PaltiKerr.py
@@ -0,0 +1,13 @@
+# Converting Celsius Temperatures to Fahrenheit 
+# 24 Sept 2018
+# CTI-110 P2HW1 Celsius Fahrenheit Converter
+# Palti Kerr
+
+# Get the Fahrenheit Temperature from Celsius Temperature (C).
+C = float(input('Enter the celsius temperature: '))
+
+# Calculate the Fahrenheit Temperature (F)
+F = ((9/5) * C) + 32
+
+# Display  the Fahrenheit Temperature
+print (str(C) + ' is ' + str(F) + ' degrees fahrenheit ')
