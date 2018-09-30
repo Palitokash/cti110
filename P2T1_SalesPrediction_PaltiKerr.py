4fd59b3269dd0b190e20f9933b04a0d2b2bae61d
Palitokash <43505918+Palitokash@users.noreply.github.com>
2018-09-30 17:08:23 -0400

    Add files via upload
    First submission!

---
+++ P2T1_SalesPrediction_PaltiKerr.py
@@ -0,0 +1,15 @@
+# Profit Calculator
+# 24 Sept 2018
+# CTI-110 P2T1 - Sales Prediction
+# Palti Kerr
+#
+
+# Get the porjected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# claculate the porfit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the porfit.
+print ('The profit is $', format(profit, ',.2f'))
+
